7f6d374b4f91ba87f90198fc338f6dd99aef44cc
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-11 22:04:05 +0300

    defining of properties on ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = 'aenshtyn'
+  usernames:string[];
+
+  constructor(){
+      this.usernames = ['mohamed', 'aenshtyn']
+}
 }
