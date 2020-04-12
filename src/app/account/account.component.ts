9fc99f786af53b3c0a02b18040ad1d22c4000096
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-12 15:55:55 +0300

    added error handlers for the http client

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { Repository } from '../repository';
 import { AccountService } from '../account-service/account.service';
+import { HttpClient } from '@angular/common/http';
+import { Profile } from '../github-class/profile'
 
 @Component({
   selector: 'app-account',
@@ -12,10 +14,21 @@ import { AccountService } from '../account-service/account.service';
 export class AccountComponent implements OnInit {
 
   usernames : User[] ;
+  profile : Profile;
 
-  constructor(accountService:AccountService) {
-  this.usernames = accountService.getUsers()
- }
+  this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+
+      this.quote = new Quote(data.author, data.quote)
+    },err=>{
+        this.quote = new Quote("Winston Churchill","Never never give up!")
+        console.log("An error occurred")
+    })
+
+
+
+  // constructor(accountService:AccountService, private http:HttpClient ) {
+  // this.usernames = accountService.getUsers()
+
 // = [
 //
 // new User (1,'Mohamed','Mohamed','Mohamed','Mohamed','Mohamed',),
@@ -29,12 +42,20 @@ export class AccountComponent implements OnInit {
 //
 // ];
 
-toggleDetails(index){
-  this.usernames[index].showRepo = !this.usernames[index].showRepo;
-}
+// toggleDetails(index){
+//   this.usernames[index].showRepo = !this.usernames[index].showRepo;
+// }
 
 
   ngOnInit() {
-  }
 
-}
+    interface ApiResponse{
+    name:string;
+    quote:string;
+  };
+
+  // this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+  //       // Succesful API request
+  //       this.profile = new Profile(data.name, data.quote)
+  //     })
+    }
